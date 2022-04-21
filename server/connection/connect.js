b93986178591a136db7f7815b5e37f286fed2191
kajlasanamdeep <kajlasanamdeep@gmail.com>
2022-04-21 10:44:20 +0530

    test

--- server/connection/connect.js
+++ server/connection/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 var connect = ()=>{
-    return mongoose.connect(process.env.dbURL,()=>{
+    return mongoose.connect('mongodb://127.0.0.1:27017/TaxiBookingDatabase',()=>{
         console.log("Database Connected!")
     });
 }
