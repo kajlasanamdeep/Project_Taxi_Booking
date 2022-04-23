437d074454c6d3af85ca69dda40db58216256fa3
kajlasanamdeep <kajlasanamdeep@gmail.com>
2022-04-23 18:23:05 +0530

    test

--- server/connection/connect.js
+++ server/connection/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 var connect = ()=>{
-    return mongoose.connect("mongodb+srv://root:root@cluster0.uj89a.mongodb.net/TaxiBookingDatabase?retryWrites=true&w=majority",(err,connect)=>{
+    return mongoose.connect("mongodb+srv://root:root@cluster0.t3p0j.mongodb.net/CabBookingDatabase?retryWrites=true&w=majority",(err,connect)=>{
         if (err) throw err;
         else if(connect){
         console.log("Database Connected!")
